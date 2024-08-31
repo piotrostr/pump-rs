5722676f31fdce0c4249bb0c712e07647946807c
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2024-08-31 18:40:48 +0200

    no compute budget?

--- src/pump_service.rs
+++ src/pump_service.rs
@@ -204,7 +204,7 @@ pub async fn _handle_pump_buy(
         lamports,
     )?;
     let mut ixs = vec![];
-    ixs.append(&mut make_compute_budget_ixs(1000069, 72014));
+    // ixs.append(&mut make_compute_budget_ixs(1000069, 72014));
     ixs.append(&mut pump::_make_buy_ixs(
         wallet.pubkey(),
         pump_buy_request.mint,
